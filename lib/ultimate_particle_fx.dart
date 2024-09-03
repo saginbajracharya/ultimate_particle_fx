560ef2e48c9c7ae8a0bd56f4b63f2be00cb089c8
sagin bajracharya <saginbajracharya09@gmail.com>
2024-09-03 23:49:42 +0545

    [Fix]::example

--- lib/ultimate_particle_fx.dart
+++ lib/ultimate_particle_fx.dart
@@ -1,7 +1,32 @@
 library ultimate_particle_fx;
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class UltimateParticleFx extends StatefulWidget {
+  const UltimateParticleFx({
+    super.key,
+  });
+
+  @override
+  UltimateParticleFxState createState() => UltimateParticleFxState();
+}
+
+class UltimateParticleFxState extends State<UltimateParticleFx> with TickerProviderStateMixin{
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget (UltimateParticleFx oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
 }
